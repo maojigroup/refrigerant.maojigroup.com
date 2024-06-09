45b68cc621d4a8e7da28c25d362c3e759a30da21
nilennoct <hfdjf007@163.com>
2024-06-09 22:27:15 +0800

    feat: add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  site: {
+    url: 'https://refrigerant.maojigroup.com',
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-headlessui',
     'nuxt-icon',
     '@nuxt/content',
+    '@nuxtjs/sitemap',
   ],
   nitro: {
     preset: 'github-pages',
