9b5b651809363aaf78c5de2b10b652e58ccd7722
nilennoct <hfdjf007@163.com>
2024-06-13 18:52:43 +0800

    feat: redirect legacy URLs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,13 @@ export default defineNuxtConfig({
     },
   },
   content: {},
+  routeRules: {
+    '/Chlorodifluoromethane_R22/': { redirect: '/products/r22/' },
+    '/tetrafluoroethane_R134a/': { redirect: '/products/r134a/' },
+    '/Mixed_refrigerant_R404A/': { redirect: '/products/r404a/' },
+    '/Mixed_refrigerant_R407C/': { redirect: '/products/r407c/' },
+    '/Mixed_refrigerant_R410A/': { redirect: '/products/r410a/' },
+    '/Mixed_refrigerant_R507/': { redirect: '/products/r507/' },
+    '/HFO_R1234yf/': { redirect: '/products/r1234yf/' },
+  },
 });
